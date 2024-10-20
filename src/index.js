83ef8bd54e2f462a0887cc6821c1197a354d386d
BuildsByPavan <pavankumardasari8639@gmail.com>
2024-10-20 23:03:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <BrowserRouter>
    <Routes>
-    <Route path='/Login' Component={Login}/>
+    <Route path='/' Component={Login}/>
    </Routes>
    </BrowserRouter>
 );
