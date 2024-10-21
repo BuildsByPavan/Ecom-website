afbae3e2a9c12e953615ae517d406bff39be1455
Dasari Pavan Kumar <pavankumardasari8639@gmail.com>
2024-10-21 11:24:02 +0530

    Added new courses

---
+++ src/my_courses/Loadcourses.js
@@ -0,0 +1,14 @@
+function Loadcourses() {
+    return (
+        <>
+            <h1>
+                1 React JS
+            </h1>
+            <h1>
+                2 Java Spring boot
+            </h1>
+        </>
+    );
+}
+
+export default Loadcourses;
